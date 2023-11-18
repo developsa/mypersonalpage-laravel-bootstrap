0510f988eb7aa2e4df9df0d7e1b526654ff7b88f
Semih Sağlam <19155012@mersin.edu.tr>
2023-11-18 23:46:20 +0300

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,414 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Semih Sağlam</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="/assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+    <!--  Header  -->
+    <header id="header">
+        <div class="d-flex flex-column">
+
+            <div class="profile">
+                <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
+                <h1 class="text-light"><a href="/">Semih Sağlam</a></h1>
+                <div class="social-links mt-3 text-center">
+
+                    <a href="https://www.facebook.com/profile.php?id=100007657786092" class="facebook"><i
+                            class="bx bxl-facebook"></i></a>
+                    <a href="https://github.com/developsa" class="google-plus"><i class="bx bxl-github"></i></a>
+                    <a href="https://www.linkedin.com/in/semih-sa%C4%9Flam-ab2a23218/" class="linkedin"><i
+                            class="bx bxl-linkedin"></i></a>
+                    <a href="https://api.whatsapp.com/send/?phone=5365267506&text&type=phone_number&app_absent=0"
+                        class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+                </div>
+            </div>
+
+            <nav id="navbar" class="nav-menu navbar">
+                <ul>
+                    <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Ana
+                                Sayfa</span></a></li>
+                    <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>Hakkımda</span></a>
+                    </li>
+                    <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i>
+                            <span>Kariyerim</span></a></li>
+                    <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i>
+                            <span>Portfolyo</span></a></li>
+                    <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i>
+                            <span>İletişim</span></a></li>
+                </ul>
+            </nav><!-- .nav-menu -->
+        </div>
+    </header><!-- Header Bitimi -->
+
+    <!--  Hero Bölümü ======= -->
+    <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="hero-container" data-aos="fade-in">
+            <h1>Semih Sağlam</h1>
+            <p>I'm <span class="typed" data-typed-items="Engineer ,Full Stack Developer,"></span></p>
+        </div>
+    </section><!-- Hero bitiş Hero -->
+
+    <main id="main">
+
+        <!--  Hakkında Bölümü  -->
+        <section id="about" class="about">
+            <div class="container">
+                <div class="section-title">
+                    <h2>Hakkımda</h2>
+                    <p>Mersin Üniversitesi Bilgisayar Mühendisliği bölümünde okuyorum. </p>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4" data-aos="fade-right">
+                        <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                        <h3>Full Stack Web Developer.</h3>
+
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <ul>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Doğum Tarihi:</strong> <span>05 July
+                                            2000</span></li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong>
+                                        <span>www.saglamsite.com</span>
+                                    </li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Telefon:</strong>
+                                        <span>05365267506</span>
+                                    </li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Şehir:</strong>
+                                        <span>Nevşehir,Türkiye</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-6">
+                                <ul>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Yaş:</strong> <span>23</span></li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Eğitim:</strong> <span>Lisans</span>
+                                    </li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong>
+                                        <span>semih502010@hotmail.com</span>
+                                    </li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong>
+                                        <span>Mümkün</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- Hakkında bölümünün sonu -->
+
+
+        <!-- Özellikler Bölümü -->
+        <section id="skills" class="skills section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>Ne yapıyorum</h2>
+                    <p>Yazılımla uğraşmaktan çok keyif alıyorum bu yüzden yeni şeyler öğrenmek beni çok mutlu
+                        ediyor.Neler yaptığımı da tabloda belirttim </p>
+                </div>
+
+                <div class="row skills-content">
+
+                    <div class="col-lg-6" data-aos="fade-up">
+
+                        <div class="progress">
+                            <span class="skill">HTML <i class="val">75%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                        <div class="progress">
+                            <span class="skill">CSS <i class="val">75%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                        <div class="progress">
+                            <span class="skill">Bootstrap<i class="val">100%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <div class="progress">
+                            <span class="skill">React<i class="val">75%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+                        <div class="progress">
+                            <span class="skill">Asp.NetCore<i class="val">70%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <div class="progress">
+                            <span class="skill">Node.js <i class="val">70%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+
+                        <div class="progress">
+                            <span class="skill">MSSQL<i class="val">75%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <div class="progress">
+                            <span class="skill">Laravel<i class="val">75%</i></span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- Özellikler Bölümünün bitişi -->
+
+        <!-- ======= Resume Section ======= -->
+        <section id="resume" class="resume">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>Kariyerim</h2>
+                    <p> Gerekli bilgiler aşağıda yazmaktadır</p>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-6" data-aos="fade-up">
+                        <h3 class="resume-title">Eğitim</h3>
+                        <div class="resume-item pb-0">
+                            <h4>Bilgisayar Mühendisliği</h4>
+                            <h5>2019 - 2024</h5>
+                            <p><em>Mersin Üniversitesi</em></p>
+                        </div>
+
+
+                    </div>
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                        <h3 class="resume-title">Deneyim</h3>
+                        <div class="resume-item">
+                            <h4>Nevşehir Üniversitesi Bilgi İşlem Daire Başkanlığı </h4>
+                            <h5>Temmuz 2023 - Ağustos 2023</h5>
+
+                            <ul>
+                                <li>Stajımı PHP Laravel üzerine yaptım </li>
+                                <li>Hazır şablon kullanıp web siteleri tasarladım. </li>
+                                <li>Projelerimize Veritabanı ekleyip Crud işlemleri yapıldı</li>
+
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- End Resume Section -->
+
+        <!-- Portfolyo Bölümü -->
+        <section id="portfolio" class="portfolio section-bg">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>Yaptığım Projeler</h2>
+                    <p>Yaptığım Projeler Oldukça ekleme yapacağım</p>
+                </div>
+
+                <div class="row" data-aos="fade-up">
+                    <div class="col-lg-12 d-flex justify-content-center">
+                        <ul id="portfolio-flters">
+                            <li data-filter="*" class="filter-active">Tümü</li>
+                            <li data-filter=".filter-app">Diğer Sayfalar </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                        <div class="portfolio-wrap">
+                            <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+                            <div class="portfolio-links">
+                                <a href="assets/img/profile-img.jpg" data-gallery="portfolioGallery"
+                                    class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
+                                <a href="#" title="More Details"><i class="bx bx-link"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+                </div>
+
+            </div>
+        </section><!--Portfolyo Bölümü -->
+
+
+
+        <!-- ======= İletişim Bölümü ======= -->
+        <section id="contact" class="contact">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>İletişim</h2>
+                    <p>Website işleriniz titizlikle yapılmaktadır,tek yapmanız gereken benimle iletişim kurmanız</p>
+                </div>
+
+                <div class="row" data-aos="fade-in">
+
+                    <div class="col-lg-5 d-flex align-items-stretch">
+                        <div class="info">
+                            <div class="address">
+                                <i class="bi bi-geo-alt"></i>
+                                <h4>Konum:</h4>
+                                <p>Nevşehir Üniversitesi</p>
+                            </div>
+
+                            <div class="email">
+                                <i class="bi bi-envelope"></i>
+                                <h4>Email:</h4>
+                                <p>semih502010@hotmail.com</p>
+                            </div>
+
+                            <div class="phone">
+                                <i class="bi bi-phone"></i>
+                                <h4>Telefon:</h4>
+                                <p>05365267506</p>
+                            </div>
+
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24924.136583891286!2d34.74624831444593!3d38.65998316297724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x152a6589d5c110b1%3A0x16bea5bb2b47d3d1!2zTmV2xZ9laGlyIEhhY8SxIEJla3RhxZ8gVmVsaSDDnG5pdmVyc2l0ZXNp!5e0!3m2!1str!2str!4v1689073745971!5m2!1str!2str"
+                                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    <label for="name">Adınız</label>
+                                    <input type="text" name="name" class="form-control" id="name"
+                                        required>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="name">Mail Adresiniz</label>
+                                    <input type="email" class="form-control" name="email" id="email"
+                                        required>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="name">Konu</label>
+                                <input type="text" class="form-control" name="subject" id="subject" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="name">Mesajınız</label>
+                                <textarea class="form-control" name="message" rows="10" required></textarea>
+                            </div>
+                            <div class="my-3">
+                                <div class="loading">Loading</div>
+                                <div class="error-message"></div>
+                                <div class="sent-message">Mesajınız gönderildi.Teşekkürler</div>
+                            </div>
+                            <div class="text-center"><button type="submit">Mesaj Gönder</button></div>
+                        </form>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+    </main>
+
+    <!-- Footer  -->
+    <footer id="footer">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>2023</span></strong>
+            </div>
+            <div class="credits">
+                Tasarlayan Semih Sağlam </a>
+            </div>
+        </div>
+    </footer>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/typed.js/typed.umd.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
